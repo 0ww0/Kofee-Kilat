e94d067f2ca48367dd97f41350494bed77a9fed4
Was War <was.war+github@outlook.com>
2024-01-11 11:47:03 +0800

    update

--- resources/views/Composables/useText.js
+++ resources/views/Composables/useText.js
@@ -5,9 +5,11 @@ export default function useText() {
     const capitalize = (text) =>
         text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 
-    const reverse = (text) => text.split("").reverse().join("");
+    const reverse = (text) => 
+        text.split("").reverse().join("");
 
-    const removeSpaces = (text) => text.replace(/\s/g, "");
+    const removeSpaces = (text) => 
+        text.replace(/\s/g, "");
 
     const addEllipsis = (text, length) =>
         text.length > length ? text.substring(0, length) + "..." : text;
